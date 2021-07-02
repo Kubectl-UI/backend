56b51d9ad51714000045f4ebceaa2dd093278b39
bemijonathan <bemijonathan@gmail.com>
2021-07-02 07:25:18 +0100

    moveing front end to electron

--- electron-frontend/src/components/sidebar.tsx
+++
@@ -1,6 +0,0 @@
-import React, { useState } from 'react';
-
-export default function Sidebar() {
-  const [list] = useState(['Pods', 'Deployments', 'Services', 'namespaces']);
-  return <div> nav </div>;
-}
--- electron-frontend/src/pages/Home.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Home() {
-  return <div className="page">Welcome to Kubectl UI </div>;
-}
